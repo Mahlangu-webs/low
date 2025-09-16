381bc1d375174e96e35c5ab738b99d4e1f1f8176
Mahlangu-webs <MAHLANGUSIYABONGA30@ICLOUD.COM>
2025-09-16 09:12:48 +0200

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -19,7 +19,7 @@ const About = forwardRef<HTMLElement, AboutProps>(({ profileImage }, ref) => {
                 <div className="flex flex-col md:flex-row items-center gap-12 lg:gap-16">
                     <div className="w-full md:w-1/3 flex justify-center">
                       <img
-  src="/WhatsApp Image 2025-09-11 at 14.08.42.jpeg"
+  src="WhatsApp Image 2025-09-11 at 14.08.42.jpeg"
   alt="Siyabonga Mahlangu" 
   className="rounded-full shadow-2xl object-cover w-64 h-64 md:w-80 md:h-80" 
 />
